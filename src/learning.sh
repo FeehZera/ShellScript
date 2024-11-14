bfd3f3caf505fa37428f42c4a9e90f8dbb4c3e2c
Filipe Schneider <filipeschneider1@gmail.com>
2024-11-14 00:58:36 -0300

    logo

---
+++ src/learning.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+x=25
+
+# readonly torna constante
+readonly y=25
+
+# unset remove valor atribuido
+unset z=25
+
+#local varias locais dentro de um bloco
+local a=10
+
+echo $x $y $z $a
+
+#
